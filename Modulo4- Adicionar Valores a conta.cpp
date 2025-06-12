7037bfb66a0a6fdda9767688e982b497a2d82175
MarySants <122947costa@gmail.com>
2025-06-12 17:17:17 -0300

    Update Modulo4- Adicionar Valores a conta.cpp

--- Modulo4- Adicionar Valores a conta.cpp
+++ Modulo4- Adicionar Valores a conta.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-extern void registrarOperacao(const char *tipo, float valor);
-void realizarDeposito(float *saldo) {
-    float deposito;
+extern void registrarOperacao(const char *tipo, float valor); // Chama a funcao externa
+void realizarDeposito(float *saldo) { // As alteracoes feitas vao modificar a variavel saldo
+    float deposito; // Variavel para armazenar o valor depositado
     printf("Informe o valor a ser depositado: ");
-    scanf("%f", &deposito);
+    scanf("%f", &deposito); // Recolhe e guarda o valor na variavel deposito
 
-    if (deposito > 0) {
-        *saldo += deposito;
-        registrarOperacao("Deposito", deposito);
+    if (deposito > 0) { // Verifica se o valor depositado e positivo
+        *saldo += deposito; // Adiciona o valor ao saldo 
+        registrarOperacao("Deposito", deposito); // Registra o deposito ao historico
         printf("Deposito de R$ %.2f realizado com sucesso\n", deposito);
         printf("Saldo atual: %.2f\n", *saldo);
     } else {
