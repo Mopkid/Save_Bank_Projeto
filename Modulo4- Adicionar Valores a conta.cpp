0e3690c6826e35760be8c34773eee46e92f9ff45
MarySants <122947costa@gmail.com>
2025-06-12 16:15:42 -0300

    Update Modulo4- Adicionar Valores a conta.cpp

--- Modulo4- Adicionar Valores a conta.cpp
+++ Modulo4- Adicionar Valores a conta.cpp
@@ -1,19 +1,17 @@
 #include <stdio.h>
-  int main(){
-  float saldo = 1000;
-  float deposito;
 
-   printf("Saldo atual: R$ %.2f\n", saldo); //Vai mostrar o saldo atual
-   printf("Informe o valor a ser Depositado: ");
-   scanf("%f", &deposito);
+extern void registrarOperacao(const char *tipo, float valor);
 
-   //verificando se o valor e positivo para adicionar a conta 
-   if (deposito > 0){
-   saldo += deposito; //adiciona o valor do deposito ao saldo
-    printf ("Deposito de R$ %.2f realizado com sucesso\n", deposito);
-    printf("Novo saldo: R$ %.2f\n", saldo); //mostrando o novo valor do saldo
-} else{ 
-    printf("O valor a ser depositado deve ser positivo!\n");
-}
-return 0; //Ass: Maria Clara
-}
+void realizarDeposito(float *saldo) {
+    float deposito;
+    printf("Informe o valor a ser depositado: ");
+    scanf("%f", &deposito);
+
+    if (deposito > 0) {
+        *saldo += deposito;
+        registrarOperacao("Depósito", deposito);
+        printf("Depósito de R$ %.2f realizado com sucesso\n", deposito);
+    } else {
+        printf("O valor a ser depositado deve ser positivo!\n");
+    }
+} // Ass: Maria Clara
